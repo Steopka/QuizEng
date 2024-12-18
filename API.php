14a3173af96822b5ff55a4dfa80975bc77b2530d
Steopka <magugo384@gmail.com>
2024-12-18 16:37:11 +0300

    готовое API

--- API.php
+++ API.php
@@ -1,58 +1,60 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: text/html; charset=utf-8');
 
 // Параметры подключения к базе данных
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "admins_and_users";
+$dbname = "bd";
 
 // Создание подключения
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Проверка подключения
 if ($conn->connect_error) {
-    die(json_encode(["error" => "Подключение не удалось: " . $conn->connect_error]));
+    die("Подключение не удалось: " . $conn->connect_error);
 }
 
-// Получение данных из запроса
-$data = json_decode(file_get_contents('.json'), true);
+$filePath = 'a.json'; // Используйте относительный путь
 
-// Определение типа запроса
-$request_type = $_SERVER['REQUEST_METHOD'];
+if (file_exists($filePath)) {
+    $data = json_decode(file_get_contents($filePath), true);
 
-if ($request_type == 'POST') {
     if (isset($data['type']) && $data['type'] == 'user') {
         // Добавление пользователя
         $first_name = $data['first_name'];
         $last_name = $data['last_name'];
         $unique_id = uniqid();
 
-        $sql = "INSERT INTO users (first_name, last_name, unique_id) VALUES ('$first_name', '$last_name', '$unique_id')";
+        $sql = $conn->prepare("INSERT INTO users (first_name, last_name, unique_id) VALUES (?, ?, ?)");
+        $sql->bind_param("sss", $first_name, $last_name, $unique_id);
 
-        if ($conn->query($sql) === TRUE) {
-            echo json_encode(["message" => "Пользователь добавлен успешно", "unique_id" => $unique_id]);
+        if ($sql->execute()) {
+            echo "Пользователь добавлен успешно. Уникальный ID: " . $unique_id;
         } else {
-            echo json_encode(["error" => "Ошибка: " . $sql . "<br>" . $conn->error]);
+            echo "Ошибка: " . $sql->error;
         }
+        $sql->close();
     } elseif (isset($data['type']) && $data['type'] == 'admin') {
         // Добавление администратора
         $first_name = $data['first_name'];
         $last_name = $data['last_name'];
         $unique_id = uniqid();
 
-        $sql = "INSERT INTO admins (first_name, last_name, unique_id) VALUES ('$first_name', '$last_name', '$unique_id')";
+        $sql = $conn->prepare("INSERT INTO admins (first_name, last_name, unique_id) VALUES (?, ?, ?)");
+        $sql->bind_param("sss", $first_name, $last_name, $unique_id);
 
-        if ($conn->query($sql) === TRUE) {
-            echo json_encode(["message" => "Администратор добавлен успешно", "unique_id" => $unique_id]);
+        if ($sql->execute()) {
+            echo "Администратор добавлен успешно. Уникальный ID: " . $unique_id;
         } else {
-            echo json_encode(["error" => "Ошибка: " . $sql . "<br>" . $conn->error]);
+            echo "Ошибка: " . $sql->error;
         }
+        $sql->close();
     } else {
-        echo json_encode(["error" => "Неверный тип запроса"]);
+        echo "Неверный тип запроса";
     }
 } else {
-    echo json_encode(["error" => "Метод запроса не поддерживается"]);
+    echo "Файл не найден";
 }
 
 // Закрытие подключения
